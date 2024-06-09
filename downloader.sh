0630c08af1fd034536450bd2152e92a759d85a85
Milan Petrovic <5rovicmilan8@gmail.com>
2024-06-09 13:00:32 +0200

    proper file handling in shell script

--- downloader.sh
+++ downloader.sh
@@ -2,16 +2,26 @@
 
 DIR="$HOME/projects/yt-scraper"
 
-"$DIR/main"
+"$DIR/reader"
 
 node "$DIR/index.js" 
 
 soung_count=$(wc -l < "$DIR/music_links")
-echo "Downloading $soung_count songs"
 
-while read -r line; do
-    yt-dlp --no-write-description -q --no-playlist --extract-audio --add-metadata --audio-format mp3 "$line"
-done < "$DIR/music_links" 
+if [[ "$soung_count" -eq 0 ]]; then
+    echo "No songs to download"
+    exit 1
+else
+    echo "Downloading $soung_count songs"
+fi
+
+if [[ -s "$DIR/music_links" ]]; then
+    while read -r line; do
+        yt-dlp --no-write-description -q --no-playlist --extract-audio --add-metadata --audio-format mp3 "$line"
+    done < "$DIR/music_links" 
+else
+    exit 1
+fi
 
 echo "Done downloading songs"
 echo "Clearing links file"
@@ -19,7 +29,13 @@ echo "Clearing links file"
 > "$DIR/music_links" 
 > "$DIR/queries" 
 
-
 echo "Moving songs to Music folder"
 
-mv *.mp3 "$HOME/Music/"
+count=$(ls -1 *.mp3 2>/dev/null | wc -l)
+
+if [[ "$count" -ne 0 ]]; then
+    mv *.mp3 ~/Music
+    echo "Moved $count songs to Music folder"
+else
+    echo "No songs to move"
+fi
